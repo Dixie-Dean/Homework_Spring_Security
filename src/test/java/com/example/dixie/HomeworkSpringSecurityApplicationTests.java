dc1bed7f997bfc8a722d38f4cce31666dda64d59
Dixie Dean <d.w.sokolov26@gmail.com>
2023-11-13 16:40:17 +0300

    Initial Commit

---
+++ src/test/java/com/example/dixie/HomeworkSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dixie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

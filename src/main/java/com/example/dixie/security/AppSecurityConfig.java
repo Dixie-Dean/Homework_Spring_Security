f7347e88100757e6a56ea9e1476cbe8f93d4f930
Dixie Dean <d.w.sokolov26@gmail.com>
2023-11-14 14:49:38 +0300

    Add security configuration

--- src/main/java/com/example/dixie/security/AppSecurityConfig.java
+++ src/main/java/com/example/dixie/security/AppSecurityConfig.java
@@ -1,2 +1,44 @@
-package com.example.dixie.security;public class AppSecurityConfig {
+package com.example.dixie.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests (authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/api/greet").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/farewell").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/ban").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/delete-message").hasAnyRole("ADMIN", "MODERATOR")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User.withDefaultPasswordEncoder().username("User").password("1234").roles("USER").build());
+        manager.createUser(
+                User.withDefaultPasswordEncoder().username("Admin").password("1234").roles("ADMIN").build());
+        manager.createUser(
+                User.withDefaultPasswordEncoder().username("Moder").password("1234").roles("MODERATOR").build());
+        return manager;
+    }
 }

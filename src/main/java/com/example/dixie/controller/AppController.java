108b1b3fbc4f8df2386d8f307ea47459841d2587
Dixie Dean <d.w.sokolov26@gmail.com>
2023-11-14 14:52:09 +0300

    Controller refactoring

--- src/main/java/com/example/dixie/controller/AppController.java
+++ src/main/java/com/example/dixie/controller/AppController.java
@@ -3,7 +3,6 @@ package com.example.dixie.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController

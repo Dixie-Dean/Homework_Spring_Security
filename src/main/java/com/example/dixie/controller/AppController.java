a627a4f36df7e1b488e42cc0f2c800c68c64e02e
Dixie Dean <d.w.sokolov26@gmail.com>
2023-11-14 14:49:08 +0300

    Add new methods to controller

--- src/main/java/com/example/dixie/controller/AppController.java
+++ src/main/java/com/example/dixie/controller/AppController.java
@@ -3,6 +3,7 @@ package com.example.dixie.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,15 @@ public class AppController {
     @GetMapping("/farewell")
     public ResponseEntity<String> tellFarewell() {
         return ResponseEntity.ok("Fare thee well, user! See you soon!");
+    }
+
+    @GetMapping("/ban")
+    public ResponseEntity<String> ban() {
+        return ResponseEntity.ok("User was banned!");
+    }
+
+    @GetMapping("/delete-message")
+    public ResponseEntity<String> deleteMessage() {
+        return ResponseEntity.ok("Message was deleted");
     }
 }

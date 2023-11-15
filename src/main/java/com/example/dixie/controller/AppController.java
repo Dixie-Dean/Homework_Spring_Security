41af46662079b8ad84e26d9c283c9d6911120f40
Dixie Dean <d.w.sokolov26@gmail.com>
2023-11-15 16:39:21 +0300

    Add new methods to controller

--- src/main/java/com/example/dixie/controller/AppController.java
+++ src/main/java/com/example/dixie/controller/AppController.java
@@ -1,8 +1,12 @@
 package com.example.dixie.controller;
 
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,4 +32,29 @@ public class AppController {
     public ResponseEntity<String> deleteMessage() {
         return ResponseEntity.ok("Message was deleted");
     }
+
+    @GetMapping("/read")
+    @Secured("ROLE_READ")
+    public ResponseEntity<String> readMethodOnly() {
+        return ResponseEntity.ok("READ");
+    }
+
+    @GetMapping("/write")
+    @RolesAllowed("WRITE")
+    public ResponseEntity<String> writeMethodOnly() {
+        return ResponseEntity.ok("WRITE");
+    }
+
+    @GetMapping("/write-delete")
+    @PreAuthorize("hasAnyRole('ROLE_WRITE', 'ROLE_DELETE')")
+    public ResponseEntity<String> writeOrDeleteMethod() {
+        return ResponseEntity.ok("PreAuthorize: WRITE or DELETE");
+    }
+
+    @GetMapping("/username")
+    @PreAuthorize("#username == authentication.principal.username")
+    public ResponseEntity<String> checkUsername(@RequestParam String username) {
+        return ResponseEntity.ok(username + " entered!");
+    }
+
 }
